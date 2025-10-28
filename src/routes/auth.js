098f0d0440e08a3420a234e8b148e485bae1dbf6
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2025-10-28 16:46:40 +0500

    format

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
-const validate = require('../middleware/validate');
-const validators = require('../middleware/validators');
+const authController = require("../controllers/authController");
+const validate = require("../middleware/validate");
+const validators = require("../middleware/validators");
 const { getAllFields } = require("../controllers/fieldController");
 
-router.post('/register', validate(validators.register), authController.register);
-router.post('/login', validate(validators.login), authController.login);
-router.post('/socialLoginSignUp', validate(validators.socialAuth), authController.socialAuth);
-router.post('/logout', validate(validators.logout), authController.logout);
-router.post('/forgot-password', validate(validators.forgotPassword), authController.forgotPassword);
-router.post('/verify-reset-code', validate(validators.verifyResetCode), authController.verifyResetCode);
-router.post('/reset-password', validate(validators.resetPassword), authController.resetPassword);
+router.post(
+  "/register",
+  validate(validators.register),
+  authController.register
+);
+router.post("/login", validate(validators.login), authController.login);
+router.post(
+  "/socialLoginSignUp",
+  validate(validators.socialAuth),
+  authController.socialAuth
+);
+router.post("/logout", validate(validators.logout), authController.logout);
+router.post(
+  "/forgot-password",
+  validate(validators.forgotPassword),
+  authController.forgotPassword
+);
+router.post(
+  "/verify-reset-code",
+  validate(validators.verifyResetCode),
+  authController.verifyResetCode
+);
+router.post(
+  "/reset-password",
+  validate(validators.resetPassword),
+  authController.resetPassword
+);
 router.get("/getFields", getAllFields);
 module.exports = router;
