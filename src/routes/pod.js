f52ce406cdf11e1386b9f96c218a67a72a1561f4
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2025-11-06 09:53:49 +0500

    get pods api not found

--- src/routes/pod.js
+++ src/routes/pod.js
@@ -4,6 +4,7 @@ const podController = require("../controllers/podController");
 const auth = require("../middleware/auth");
 
 router.post("/createPod", auth, podController.createPod);
+router.get("/getPods", auth, podController.getPods);
 router.put("/editPod/:id", auth, podController.editPod);
 router.delete("/deletePod/:id", auth, podController.deletePod);
 router.get("/searchUsers", auth, podController.searchUsers);
